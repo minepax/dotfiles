caa5a4280a7f37936497520be7e31f979b4c53f2
Maheshwar Rathakrishnan <maherbecs@gmail.com>
2026-08-10 18:18:01 +0400

    Backup: 2026-08-10 18:18:01

--- local/bin/dots-backup.sh
+++ local/bin/dots-backup.sh
@@ -9,9 +9,13 @@ echo "Syncing configs..."
 rsync -av --delete --exclude 'google-chrome' --exclude 'discord' "$CONF_DIR/fastfetch" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/hypr" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/waybar" "$DOTS_DIR/config/"
-rsync -av --delete "$CONF_DIR/swaync" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/rofi" "$DOTS_DIR/config/"
+rsync -av --delete "$CONF_DIR/swaync" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/kitty" "$DOTS_DIR/config/"
+rsync -av --delete "$CONF_DIR/gtk-3.0" "$DOTS_DIR/config/"
+rsync -av --delete "$CONF_DIR/gtk-4.0" "$DOTS_DIR/config/"
+rsync -av --delete "$CONF_DIR/qt5ct" "$DOTS_DIR/config/"
+rsync -av --delete "$CONF_DIR/qt6ct" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/zed" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/nvim" "$DOTS_DIR/config/"
 rsync -av --delete "$CONF_DIR/obs-studio" "$DOTS_DIR/config/"
@@ -30,5 +34,5 @@ else
     echo "Changes detected. Pushing to GitHub..."
     git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
     git push origin main
-    notify-send "Backup Complete" "Your dotfiles have been synced to GitHub." -i standard-icon-git
+    notify-send "Backup Complete" "Dotfiles have been synced to GitHub." -i ~/.local/share/icons/YAMIS/apps/scalable/github-desktop.svg
 fi
